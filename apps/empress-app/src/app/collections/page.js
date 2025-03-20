2bb559357ca4b4f9c1a751e5db04a97211f3c135
Chinedu Amaechi <chymez56@yahoo.com>
2025-03-20 17:33:20 -0600

    Update collection-navigator for Responsivenes

--- apps/empress-app/src/app/collections/page.js
+++ apps/empress-app/src/app/collections/page.js
@@ -142,7 +142,7 @@ export default function Collections() {
       {/* Fixed navigation bar */}
       <div
         className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ${
-          isScrolled ? "bg-white shadow-md py-3" : "bg-transparent py-5"
+          isScrolled ? "bg-white shadow-md py-1" : "bg-transparent py-3"
         }`}
       >
         <div className="max-w-7xl mx-auto pt-20 px-4 sm:px-6 lg:px-8"></div>
