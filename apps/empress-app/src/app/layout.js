1c8eea3543e36d40eddeb5cf8977ca6ce521199f
Chinedu Amaechi <chymez56@yahoo.com>
2025-03-24 08:08:13 -0600

    Add chatnot feature

--- apps/empress-app/src/app/layout.js
+++ apps/empress-app/src/app/layout.js
@@ -1,3 +1,4 @@
+import EmpressChatbot from "@/components/chatbot/empress-chatbot";
 import "./globals.css";
 
 export const metadata = {
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <EmpressChatbot />
     </html>
   );
 }
