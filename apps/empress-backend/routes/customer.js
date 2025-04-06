6934bd35ce6ed753b23324dda8746ba07dec734d
Faruq Ayomide <onaayofaruq24@gmail.com>
2025-04-06 07:16:24 -0600

    implement customer cart functionality with add, update, retrieve, and remove routes

--- apps/empress-backend/routes/customer.js
+++ apps/empress-backend/routes/customer.js
@@ -1,14 +1,24 @@
 // 3rd party modules
 import express from "express";
-import formidable from "formidable";
 
 // Custom modules
 import * as customerControllers from "../controllers/customer.js";
-
 
 /*
  * The express.Router class can be used to create modular, mountable route handlers.
  */
 const router = express.Router();
+
+// route to add a product to the cart
+router.post("/cart", customerControllers.addToCart);
+
+// route to update a product in the cart
+router.put("/cart/:productId", customerControllers.updateCart);
+
+// route to get all products in the cart
+router.get("/cart", customerControllers.getCart);
+
+// route to remove a product from the cart
+router.delete("/cart/:productId", customerControllers.removeFromCart);
 
 export default router;
